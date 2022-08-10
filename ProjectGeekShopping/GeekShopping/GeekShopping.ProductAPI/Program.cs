a99d0e1f98127be80e468711effe07a6ce9876be
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-10 12:13:24 -0300

    criando bd e primeira migration

--- ProjectGeekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,9 +1,17 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection, new MySqlServerVersion(
+        new Version(8, 0, 30))));
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
