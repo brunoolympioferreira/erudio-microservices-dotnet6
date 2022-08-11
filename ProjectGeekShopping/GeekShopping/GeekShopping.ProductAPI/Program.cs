3f1901247e0f3937cf27b1b3df3d146fcca62790
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-11 08:40:03 -0300

    finalizando api products com controller e massa de dados

--- ProjectGeekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,12 @@ var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.
     UseMySql(connection, new MySqlServerVersion(
         new Version(8, 0, 30))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
