7f780f8667de6c73081c59088b9683d661a16bf4
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-15 14:35:10 -0300

    confiruando user roles e users e criando carga de dados

--- ProjectGeekShopping/GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,23 +1,64 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Initializer;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection, new MySqlServerVersion(
+        new Version(8, 0, 30))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+        .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+        .AddInMemoryClients(IdentityConfiguration.Clients)
+        .AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+builderIdentity.AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+var dbInitializeService = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializer>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
+
+dbInitializeService.Initialize();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
