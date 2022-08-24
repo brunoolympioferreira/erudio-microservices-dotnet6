df53016c81f1e49753c1e563b9f3c43789c9e99f
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-24 11:24:26 -0300

    implementando logica de cupon de desconto

--- ProjectGeekShopping/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -10,13 +10,16 @@ namespace GeekShopping.Web.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICartService _cartService;
+        private readonly ICouponService _couponService;
 
 
         public CartController(IProductService productService,
-                              ICartService cartService)
+                              ICartService cartService,
+                              ICouponService couponService)
         {
             _productService = productService;
             _cartService = cartService;
+            _couponService = couponService;
         }
 
         [Authorize]
@@ -77,10 +80,19 @@ namespace GeekShopping.Web.Controllers
 
             if (response?.CartHeader != null)
             {
+                if (!string.IsNullOrEmpty(response.CartHeader.CouponCode))
+                {
+                    var coupon = await _couponService.GetCoupon(response.CartHeader.CouponCode,token);
+                    if (coupon?.CouponCode != null)
+                    {
+                        response.CartHeader.DiscountAmount = coupon.DiscountAmount;
+                    }
+                }
                 foreach (var detail in response.CartDetails)
                 {
                     response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
                 }
+                response.CartHeader.PurchaseAmount -= response.CartHeader.DiscountAmount;
             }
 
             return response;
--- ProjectGeekShopping/GeekShopping/GeekShopping.Web/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Web/Program.cs
@@ -14,9 +14,9 @@ builder.Services.AddHttpClient<ICartService, CartService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
 );
 
-//builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
-//    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
-//);
+builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
+);
 
 builder.Services.AddControllersWithViews();
 
