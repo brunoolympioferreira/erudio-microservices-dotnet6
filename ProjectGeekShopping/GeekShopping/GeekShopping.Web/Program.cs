84c414e1444d686e3224c28d801715941ff65a88
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-24 10:48:44 -0300

    configurando coupon em geekShopping web

--- ProjectGeekShopping/GeekShopping/GeekShopping.Web/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Web/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddHttpClient<ICartService, CartService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
 );
 
+//builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+//    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"])
+//);
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddAuthentication(options =>
--- ProjectGeekShopping/GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Web/Services/IServices/ICartService.cs
@@ -9,7 +9,7 @@ namespace GeekShopping.Web.Services.IServices
         Task<CartViewModel> UpdateCart(CartViewModel cart, string token);
         Task<bool> RemoveFromCart(long cartId, string token);
 
-        Task<bool> ApllyCoupon(long cartId, string token);
+        Task<bool> ApllyCoupon(CartViewModel cart, string couponCode, string token);
         Task<bool> RemoveCoupon(string userId, string token);
         Task<bool> ClearCart(string userId, string token);
         Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token);
