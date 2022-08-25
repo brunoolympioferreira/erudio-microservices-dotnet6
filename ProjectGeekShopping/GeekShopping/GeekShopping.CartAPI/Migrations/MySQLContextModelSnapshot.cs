5e8946a57363581b132632807f7cfdb76d2214db
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-25 15:06:53 -0300

    modelando entidades da api order

--- ProjectGeekShopping/GeekShopping/GeekShopping.CartAPI/Migrations/MySQLContextModelSnapshot.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.CartAPI/Migrations/MySQLContextModelSnapshot.cs
@@ -41,7 +41,7 @@ namespace GeekShopping.CartAPI.Migrations
 
                     b.HasIndex("ProductId");
 
-                    b.ToTable("cart_detail");
+                    b.ToTable("cart_detail", (string)null);
                 });
 
             modelBuilder.Entity("GeekShopping.CartAPI.Model.CartHeader", b =>
@@ -61,7 +61,7 @@ namespace GeekShopping.CartAPI.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("cart_header");
+                    b.ToTable("cart_header", (string)null);
                 });
 
             modelBuilder.Entity("GeekShopping.CartAPI.Model.Product", b =>
@@ -97,7 +97,7 @@ namespace GeekShopping.CartAPI.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("product");
+                    b.ToTable("product", (string)null);
                 });
 
             modelBuilder.Entity("GeekShopping.CartAPI.Model.CartDetail", b =>
