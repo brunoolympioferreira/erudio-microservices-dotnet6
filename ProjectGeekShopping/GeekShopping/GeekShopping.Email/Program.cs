0c2baadaa1fe616c8e71c3cc87a23f4810feffdf
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-08-31 17:13:17 -0300

    criando microsservico de email e seu bd

---
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Email/Program.cs
@@ -0,0 +1,32 @@
+using GeekShopping.Email.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+    UseMySql(connection, new MySqlServerVersion(
+        new Version(8, 0, 30))));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
