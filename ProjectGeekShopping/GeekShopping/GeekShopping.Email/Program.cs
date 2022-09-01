518bf572fa94747f98933a341dd29bb295dccdc8
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-09-01 16:44:04 -0300

    implementando direct_exchange

--- ProjectGeekShopping/GeekShopping/GeekShopping.Email/Program.cs
+++ ProjectGeekShopping/GeekShopping/GeekShopping.Email/Program.cs
@@ -1,5 +1,8 @@
+using GeekShopping.Email.MessageConsumer;
 using GeekShopping.Email.Model.Context;
+using GeekShopping.Email.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,37 @@ builder.Services.AddDbContext<MySQLContext>(options => options.
     UseMySql(connection, new MySqlServerVersion(
         new Version(8, 0, 30))));
 
+var builderContext = new DbContextOptionsBuilder<MySQLContext>();
+builderContext.UseMySql(connection, new MySqlServerVersion(
+        new Version(8, 0, 30)));
+
+builder.Services.AddSingleton(new EmailRepository(builderContext.Options));
+
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,6 +56,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
